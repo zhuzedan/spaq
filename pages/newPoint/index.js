d2fda9d4d505a3feb75fa4240b2a007c01b0fb82
zhuzedan <1031155817@qq.com>
2022-12-08 17:24:25 +0800

    新增检查点

--- pages/newPoint/index.js
+++ pages/newPoint/index.js
@@ -1,5 +1,6 @@
 // pages/newPoint/index.js
 import Toast from '@vant/weapp/toast/toast';
+var app = getApp();
 Page({
 
   /**
@@ -9,85 +10,198 @@ Page({
     latitude: null,
     longitude: null,
     showUnit: false, //单位弹层控制
-    columns: ['公益', '商业'],
-    category: ['农家乐','养老院'],
-    unit:'请选择类型',
-    sex: [{ id: "0", name: "男", checked:"true" }, { id: "1", name: "女" }],
-    name:'',
-    sexId:"0",   // 默认是0 => 男
-    sex1:'',
-    Email:'',
-    school:'',
-    index:0,
-    identity:["请选择区域","海曙区","鄞州区","江北区","镇海区","慈溪市"],
-    occupation:''
+    category: ['请选择类别', '农家乐', '养老院'],
+    type: ["请选择类型", '公益', '商业'],
+    index1: 0,
+    index2: 0,
+    index3: 0,
+    area: ["请选择区域", "海曙区", "鄞州区", "江北区", "镇海区", "慈溪市", "余姚市"],
+    occupation: '',
+    // 表单字段
+    name: '', //单位名
+    businessType: '', //类型
+    categoryCode: '', // 类别
+    areaOrgCode: '', //区域
+    streetOrgCode: '', //街道
+    connectName: '', //联系人
+    connectTel: '', //电话
   },
-  showPopup() {
-    this.setData({ show: true });
-  },
-  getUnit(e) {
-    wx.hideKeyboard();
-    this.showPopup();
-  },
-  showPopup() {
+  // 双向绑定-单位名称
+  getName: function (e) {
     this.setData({
-      showUnit: true
+      name: e.detail.value
     });
   },
-  // 弹窗关闭
-  onCloseUnit() {
+  // 双向绑定-类型选择器
+  getBusinessType: function (e) {
     this.setData({
-      showUnit: false
-    });
-  },
-  //单位选择确认
-  onConfirm(event) {
-    const {
-      picker,
-      value,
-      index
-    } = event.detail;
-    this.setData({
-      unit:value
+      index1: e.detail.value,
+      businessType: this.data.type[e.detail.value]
     })
-    this.onCloseUnit();
-
+    // console.log(this.data.index1);
+    // console.log(this.data.type[this.data.index1]);
   },
-  //单位取消选择器
-  onCancel() {
-    this.onCloseUnit();
-    console.log('value:', value)
-  },
-  onChange(event) {
-    const { picker, value, index } = event.detail;
-    Toast(`当前值：${value}, 当前索引：${index}`);
-  },
-  getForm:function(e){
-    var formdata = e.detail.value
+  // 双向绑定-类别选择器
+  getCategoryCode: function (e) {
     this.setData({
-    name:formdata.name,
-    sex1:this.data.sex[this.data.sexId].name,
-    Email:formdata.Email,
-    school:formdata.school,
-    occupation:this.data.identity[this.data.index],
-   })
+      index2: e.detail.value,
+      categoryCode: this.data.category[e.detail.value]
+    })
   },
-  radioChange:function(e){
-      this.setData({
-          sexId:e.detail.value
+  // 双向绑定-区域:
+  getAreaOrgCode: function (e) {
+    this.setData({
+      index3: e.detail.value,
+      areaOrgCode: this.data.area[e.detail.value]
+    });
+  },
+  // 双向绑定-街道
+  getStreetOrgCode: function (e) {
+    this.setData({
+      streetOrgCode: e.detail.value
+    });
+  },
+  // 双向绑定-联系人
+  getConnectName: function (e) {
+    this.setData({
+      connectName: e.detail.value
+    });
+  },
+  // 双向绑定-联系人电话
+  getConnectTel: function (e) {
+    this.setData({
+      connectTel: e.detail.value
+    });
+  },
+  // 确定按钮
+  submit() {
+    var name = this.data.name
+    var businessType = this.data.businessType
+    var categoryCode = this.data.categoryCode
+    var areaOrgCode = this.data.areaOrgCode
+    var streetOrgCode = this.data.streetOrgCode
+    var connectName = this.data.connectName
+    var connectTel = this.data.connectTel
+
+    // 判断输入内容是否空值
+    if (name == '') {
+      wx.showToast({
+        title: '单位名不能为空',
+        icon: 'none'
       })
-  },
-  bindPickerChange:function(e){
-      this.setData({
-          index: e.detail.value
+      return;
+    }
+    if (businessType == '') {
+      wx.showToast({
+        title: '请选择类型',
+        icon: 'none'
       })
+      return;
+    }
+    if (categoryCode == '') {
+      wx.showToast({
+        title: '请选择类别',
+        icon: 'none'
+      })
+      return;
+    }
+    if (areaOrgCode == '') {
+      wx.showToast({
+        title: '请选择区域',
+        icon: 'none'
+      })
+      return;
+    }
+    if (streetOrgCode == '') {
+      wx.showToast({
+        title: '请输入街道',
+        icon: 'none'
+      })
+      return;
+    }
+    if (connectName == '') {
+      wx.showToast({
+        title: '联系人不能为空',
+        icon: 'none'
+      })
+      return;
+    }
+    if (connectTel == '') {
+      wx.showToast({
+        title: '联系人电话不能为空',
+        icon: 'none'
+      })
+      return;
+    }
+    if (connectTel.length != 0 && connectTel.length != 11) { //输入的手机号不足11位提示
+      wx.showToast({
+        title: '请输入11位手机号',
+        icon: 'none'
+      })
+      return;
+    }
+    if (connectTel.length == 11) { //输入的手机号满足11位
+      //正则匹配开头是1总长度为11的号码
+      let regex  = /^(((1[35789][0-9]{1})|(15[0-9]{1})|(18[0-9]{1})|(17[0-9]{1}))+\d{8})$/
+      if (!regex.test(connectTel)) {
+        wx.showToast({
+          title: '手机号格式有误',
+          icon: 'none'
+        })
+      }
+    }
+    wx.request({
+      header: {
+        "Authorization": "Bearer " + app.globalData.userInfo.token
+      },
+      url: 'http://localhost:8082/api/app-my/insertCheckPoint',
+      data: {
+        name: this.data.name,
+        connectName: this.data.connectName,
+        connectTel: this.data.connectTel,
+        businessType: this.data.index1 - 1,
+        categoryCode: this.data.categoryCode,
+        areaOrgCode: this.data.areaOrgCode,
+        streetOrgCode: this.data.streetOrgCode,
+        address: this.data.latitude + "," + this.data.longitude
+      },
+      method: 'POST',
+      success: function (res) {
+        // console.log(res);
+        if (res.data.code == 200) {
+          wx.showModal({
+            title: '',
+            content: '确认提交吗？',
+            complete: (res) => {
+              if (res.confirm) {
+                wx.showToast({
+                  title: '提交成功',
+                  duration: 2000
+                })
+                wx.switchTab({
+                  url: '../index/index',
+                })
+              }
+            }
+          })
+
+        } else {
+          wx.showToast({
+            title: '未知错误',
+          })
+        }
+
+      }
+    })
   },
-  saveData:function(e){
-      //提交后的操作，例如将信息写入数据库等
+  bindPickerChange3: function (e) {
+    this.setData({
+      index3: e.detail.value
+    })
   },
   getLocation() {
-    
-    
+
+
   },
 
   /**
@@ -97,17 +211,17 @@ Page({
     var that = this;
     wx.getLocation({
       type: 'wgs84',
-      success (res) {
+      success(res) {
         const latitude = res.latitude
         const longitude = res.longitude
         const accuracy = res.accuracy
-        console.log(res) //将获取到的经纬度信息输出到控制台以便检查
+        // console.log(res) //将获取到的经纬度信息输出到控制台以便检查
         that.setData({ //将获取到的经度、纬度数值分别赋值给本地变量
           latitude: (res.latitude).toFixed(4),
           longitude: (res.longitude).toFixed(4)
         })
       }
-     })
+    })
   },
 
   /**
